2f01ca2c83ee20540b581ba1692e689b1e68c1dd
l0ckstreet <rsalas7793@gmail.com>
2020-05-20 16:40:55 +0800

    linked burger controls to state

--- src/components/BuildControls/BuildControls.js
+++ src/components/BuildControls/BuildControls.js
@@ -11,11 +11,18 @@ const controls = [
 
 
 const buildControls = (props) => {
+
     return (
         <div className={classes.BuildControls}>
             {controls.map(control => {
                 return (
-                <BuildControl key={control.label} label={control.label}/>
+                <BuildControl 
+                    key={control.label}
+                    label={control.label}
+                    added={()=>props.ingredientAdded(control.type)}
+                    removed={()=>props.ingredientRemoved(control.type)}
+                    disabled={props.disabled[control.type]}
+                />
                 )
             })}
         </div>
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -17,7 +17,6 @@ const burger = (props) =>{
         transformedIngredients = <p className={classes.burgerP}>Feel free to add ingredients</p>
     }
 
-    console.log(transformedIngredients)
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
