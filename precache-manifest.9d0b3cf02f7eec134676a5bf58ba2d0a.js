690b395fd02be0b827a6cb7d85d39c0a1b4c6e8b
l0ckstreet <rsalas7793@gmail.com>
2020-05-29 22:49:41 +0800

    Updates

--- precache-manifest.9d0b3cf02f7eec134676a5bf58ba2d0a.js
+++ precache-manifest.9d0b3cf02f7eec134676a5bf58ba2d0a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c0e0f013648653f2e1e8b5b86ed1d75",
+    "revision": "d3491d841fc3ee3d43f76ef5d2374c69",
     "url": "/burger-builder/index.html"
   },
   {
-    "revision": "981af317c1046eb8acc0",
+    "revision": "f606574800845933bd7f",
     "url": "/burger-builder/static/css/main.ae8d82c6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/burger-builder/static/js/2.8d2014bc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "981af317c1046eb8acc0",
-    "url": "/burger-builder/static/js/main.139df755.chunk.js"
+    "revision": "f606574800845933bd7f",
+    "url": "/burger-builder/static/js/main.68b41b49.chunk.js"
   },
   {
     "revision": "3770ce84a4402a6d51b9",
