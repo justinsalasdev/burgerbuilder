03ede8cc293f19929f444c709b3e663eb4b08b29
l0ckstreet <rsalas7793@gmail.com>
2020-05-29 22:28:37 +0800

    Updates

---
+++ precache-manifest.9d0b3cf02f7eec134676a5bf58ba2d0a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3491d841fc3ee3d43f76ef5d2374c69",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "f606574800845933bd7f",
+    "url": "/burger-builder/static/css/main.ae8d82c6.chunk.css"
+  },
+  {
+    "revision": "1c30bf6ffbc94c234e3d",
+    "url": "/burger-builder/static/js/2.8d2014bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger-builder/static/js/2.8d2014bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f606574800845933bd7f",
+    "url": "/burger-builder/static/js/main.68b41b49.chunk.js"
+  },
+  {
+    "revision": "3770ce84a4402a6d51b9",
+    "url": "/burger-builder/static/js/runtime-main.668bfeb5.js"
+  },
+  {
+    "revision": "0413ca85383d15e6b18c1e7c0c3be5c1",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-700.0413ca85.eot"
+  },
+  {
+    "revision": "52df43632c09bff7e95a77b3c3e1d53a",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-700.52df4363.woff2"
+  },
+  {
+    "revision": "609c6693da64dba4f50c980781c767d8",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-700.609c6693.svg"
+  },
+  {
+    "revision": "b566ecc378c6ce8ff315af5d74b1981a",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-700.b566ecc3.ttf"
+  },
+  {
+    "revision": "eb6caf39a2007894d744bed7d703ff03",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-700.eb6caf39.woff"
+  },
+  {
+    "revision": "4c42cd99677919ef3236abf2b13b7c91",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-regular.4c42cd99.svg"
+  },
+  {
+    "revision": "72470a28226dcaddd1d605adfbca925c",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-regular.72470a28.ttf"
+  },
+  {
+    "revision": "b7caf2dabf19a0e38920098392679e36",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-regular.b7caf2da.woff"
+  },
+  {
+    "revision": "c2f6fde0a1f92c14920ae2270bea8c09",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-regular.c2f6fde0.woff2"
+  },
+  {
+    "revision": "deaa266128425788acf0328ba8087cd4",
+    "url": "/burger-builder/static/media/balsamiq-sans-v1-latin-regular.deaa2661.eot"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
