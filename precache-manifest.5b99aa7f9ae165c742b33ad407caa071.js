b0f0a0eca0098a54d2bb6b3cf86aed7415e5187b
l0ckstreet <rsalas7793@gmail.com>
2020-07-13 14:48:06 +0800

    Updates

---
+++ precache-manifest.5b99aa7f9ae165c742b33ad407caa071.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9294744d37a09788f7cf325b3e23b25",
+    "url": "/burgerbuilder/index.html"
+  },
+  {
+    "revision": "b6c87e8adde133bf45ad",
+    "url": "/burgerbuilder/static/css/4.727f316b.chunk.css"
+  },
+  {
+    "revision": "1093dfa1c139b5aa3979",
+    "url": "/burgerbuilder/static/css/5.727f316b.chunk.css"
+  },
+  {
+    "revision": "8cfa872320b3372baa8a",
+    "url": "/burgerbuilder/static/css/6.e8021be7.chunk.css"
+  },
+  {
+    "revision": "1badda4abdc3d8003eb1",
+    "url": "/burgerbuilder/static/css/7.4d6f349a.chunk.css"
+  },
+  {
+    "revision": "21f8e71315a513a2b9b9",
+    "url": "/burgerbuilder/static/css/8.59e90ff8.chunk.css"
+  },
+  {
+    "revision": "fbd1e2ddbafe9c62a93d",
+    "url": "/burgerbuilder/static/css/9.070bd7b1.chunk.css"
+  },
+  {
+    "revision": "fce41121cc38c771640b",
+    "url": "/burgerbuilder/static/css/main.6592bf0e.chunk.css"
+  },
+  {
+    "revision": "cae473f0611d00cfe4a1",
+    "url": "/burgerbuilder/static/js/0.cf846597.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/burgerbuilder/static/js/0.cf846597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47aa50f04fe2d9db18d6",
+    "url": "/burgerbuilder/static/js/3.a772b0d7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgerbuilder/static/js/3.a772b0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c87e8adde133bf45ad",
+    "url": "/burgerbuilder/static/js/4.746344ca.chunk.js"
+  },
+  {
+    "revision": "1093dfa1c139b5aa3979",
+    "url": "/burgerbuilder/static/js/5.261729d0.chunk.js"
+  },
+  {
+    "revision": "8cfa872320b3372baa8a",
+    "url": "/burgerbuilder/static/js/6.efaa34e5.chunk.js"
+  },
+  {
+    "revision": "1badda4abdc3d8003eb1",
+    "url": "/burgerbuilder/static/js/7.c1519436.chunk.js"
+  },
+  {
+    "revision": "21f8e71315a513a2b9b9",
+    "url": "/burgerbuilder/static/js/8.ff981c29.chunk.js"
+  },
+  {
+    "revision": "fbd1e2ddbafe9c62a93d",
+    "url": "/burgerbuilder/static/js/9.b3de4da7.chunk.js"
+  },
+  {
+    "revision": "fce41121cc38c771640b",
+    "url": "/burgerbuilder/static/js/main.669ef75e.chunk.js"
+  },
+  {
+    "revision": "fcc6a8d4ba799fd0835a",
+    "url": "/burgerbuilder/static/js/runtime-main.378db64a.js"
+  },
+  {
+    "revision": "0413ca85383d15e6b18c1e7c0c3be5c1",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.0413ca85.eot"
+  },
+  {
+    "revision": "52df43632c09bff7e95a77b3c3e1d53a",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.52df4363.woff2"
+  },
+  {
+    "revision": "609c6693da64dba4f50c980781c767d8",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.609c6693.svg"
+  },
+  {
+    "revision": "b566ecc378c6ce8ff315af5d74b1981a",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.b566ecc3.ttf"
+  },
+  {
+    "revision": "eb6caf39a2007894d744bed7d703ff03",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.eb6caf39.woff"
+  },
+  {
+    "revision": "4c42cd99677919ef3236abf2b13b7c91",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.4c42cd99.svg"
+  },
+  {
+    "revision": "72470a28226dcaddd1d605adfbca925c",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.72470a28.ttf"
+  },
+  {
+    "revision": "b7caf2dabf19a0e38920098392679e36",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.b7caf2da.woff"
+  },
+  {
+    "revision": "c2f6fde0a1f92c14920ae2270bea8c09",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.c2f6fde0.woff2"
+  },
+  {
+    "revision": "deaa266128425788acf0328ba8087cd4",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.deaa2661.eot"
+  },
+  {
+    "revision": "983d8df126afb98b0e77a899f62cbefa",
+    "url": "/burgerbuilder/static/media/burger-logo.983d8df1.svg"
+  },
+  {
+    "revision": "371298c8e046498a3463fd1b6a47abfe",
+    "url": "/burgerbuilder/static/media/wood2.371298c8.jpg"
+  }
+]);
