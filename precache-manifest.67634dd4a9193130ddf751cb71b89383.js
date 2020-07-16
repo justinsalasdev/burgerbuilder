d5fb7559bee87edea17c30d224dd15c42329a3e3
Justin Rafael M. Salas <jrmsalas@meralco.com.ph>
2020-07-16 18:04:26 +0800

    Updates

---
+++ precache-manifest.67634dd4a9193130ddf751cb71b89383.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801deb8fe0b2c4a75170f7051c3f22c4",
+    "url": "/burgerbuilder/index.html"
+  },
+  {
+    "revision": "05e796fe8fc95314788d",
+    "url": "/burgerbuilder/static/css/4.90297b8c.chunk.css"
+  },
+  {
+    "revision": "238a2c32e822179cb2bf",
+    "url": "/burgerbuilder/static/css/5.e361fd96.chunk.css"
+  },
+  {
+    "revision": "8f375ccc3d2ec2adc9af",
+    "url": "/burgerbuilder/static/css/6.b176494a.chunk.css"
+  },
+  {
+    "revision": "81f005f16e0dd8f81942",
+    "url": "/burgerbuilder/static/css/7.b176494a.chunk.css"
+  },
+  {
+    "revision": "b42335ff0cc615e2d346",
+    "url": "/burgerbuilder/static/css/8.14da426c.chunk.css"
+  },
+  {
+    "revision": "1c8ffbd25ecece909ac4",
+    "url": "/burgerbuilder/static/css/9.7ef66079.chunk.css"
+  },
+  {
+    "revision": "bdd96b03d17571b1513a",
+    "url": "/burgerbuilder/static/css/main.15c0ab82.chunk.css"
+  },
+  {
+    "revision": "d93b107b270c669159cc",
+    "url": "/burgerbuilder/static/js/0.8cdedab6.chunk.js"
+  },
+  {
+    "revision": "bd49b04565accd5b6f2624c86f8385ba",
+    "url": "/burgerbuilder/static/js/0.8cdedab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7048816c5591d286ecaa",
+    "url": "/burgerbuilder/static/js/3.25fca6c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgerbuilder/static/js/3.25fca6c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e796fe8fc95314788d",
+    "url": "/burgerbuilder/static/js/4.7b679ede.chunk.js"
+  },
+  {
+    "revision": "238a2c32e822179cb2bf",
+    "url": "/burgerbuilder/static/js/5.9cdf13a2.chunk.js"
+  },
+  {
+    "revision": "8f375ccc3d2ec2adc9af",
+    "url": "/burgerbuilder/static/js/6.d435cdb6.chunk.js"
+  },
+  {
+    "revision": "81f005f16e0dd8f81942",
+    "url": "/burgerbuilder/static/js/7.97553221.chunk.js"
+  },
+  {
+    "revision": "b42335ff0cc615e2d346",
+    "url": "/burgerbuilder/static/js/8.53512aee.chunk.js"
+  },
+  {
+    "revision": "1c8ffbd25ecece909ac4",
+    "url": "/burgerbuilder/static/js/9.6c532308.chunk.js"
+  },
+  {
+    "revision": "bdd96b03d17571b1513a",
+    "url": "/burgerbuilder/static/js/main.a32b3a0e.chunk.js"
+  },
+  {
+    "revision": "fda42aacba62ce283292",
+    "url": "/burgerbuilder/static/js/runtime-main.8e1134c2.js"
+  },
+  {
+    "revision": "064cfd766353ec4ee77910542d272e38",
+    "url": "/burgerbuilder/static/media/bacon.064cfd76.svg"
+  },
+  {
+    "revision": "0413ca85383d15e6b18c1e7c0c3be5c1",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.0413ca85.eot"
+  },
+  {
+    "revision": "52df43632c09bff7e95a77b3c3e1d53a",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.52df4363.woff2"
+  },
+  {
+    "revision": "609c6693da64dba4f50c980781c767d8",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.609c6693.svg"
+  },
+  {
+    "revision": "b566ecc378c6ce8ff315af5d74b1981a",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.b566ecc3.ttf"
+  },
+  {
+    "revision": "eb6caf39a2007894d744bed7d703ff03",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-700.eb6caf39.woff"
+  },
+  {
+    "revision": "4c42cd99677919ef3236abf2b13b7c91",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.4c42cd99.svg"
+  },
+  {
+    "revision": "72470a28226dcaddd1d605adfbca925c",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.72470a28.ttf"
+  },
+  {
+    "revision": "b7caf2dabf19a0e38920098392679e36",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.b7caf2da.woff"
+  },
+  {
+    "revision": "c2f6fde0a1f92c14920ae2270bea8c09",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.c2f6fde0.woff2"
+  },
+  {
+    "revision": "deaa266128425788acf0328ba8087cd4",
+    "url": "/burgerbuilder/static/media/balsamiq-sans-v1-latin-regular.deaa2661.eot"
+  },
+  {
+    "revision": "1cbcc545bb15788c09d398f424ac71f5",
+    "url": "/burgerbuilder/static/media/burger.1cbcc545.svg"
+  },
+  {
+    "revision": "e87597a5018cb03d6a5e8d56442fa4d3",
+    "url": "/burgerbuilder/static/media/cheese.e87597a5.svg"
+  },
+  {
+    "revision": "03ca5fc04a76d98721736445e054cdb9",
+    "url": "/burgerbuilder/static/media/meat.03ca5fc0.svg"
+  },
+  {
+    "revision": "d0b42f7cafa3a7970af8d288c84b217c",
+    "url": "/burgerbuilder/static/media/notes.d0b42f7c.svg"
+  },
+  {
+    "revision": "ff190b2d0a237b834a671f3628b375e0",
+    "url": "/burgerbuilder/static/media/profile.ff190b2d.svg"
+  },
+  {
+    "revision": "95e1448524701e306fb40b0c52d94da2",
+    "url": "/burgerbuilder/static/media/salad.95e14485.svg"
+  },
+  {
+    "revision": "1efeddbdc7cc6b80ef421ea9e88e9d27",
+    "url": "/burgerbuilder/static/media/spatula.1efeddbd.svg"
+  },
+  {
+    "revision": "371298c8e046498a3463fd1b6a47abfe",
+    "url": "/burgerbuilder/static/media/wood2.371298c8.jpg"
+  }
+]);
