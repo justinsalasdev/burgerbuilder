86fcfac374771877c313fc5e59ab458f098515f0
l0ckstreet <rsalas7793@gmail.com>
2020-09-01 13:17:40 +0800

    fixed redirect

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,96 +1,100 @@
-import React, {Suspense,useEffect} from 'react';
-import Layout from '../Layout/Layout';
-import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
-import {Route, Switch,Redirect} from 'react-router-dom';
-import {useDispatch,useSelector} from 'react-redux';
-import * as actions from '../../store/actions/exports'
-import Spinner from '../../recycle/Spinner/Spinner'
-import useAlert from '../../hooks/useAlert';
-import Alert from '../../recycle/Alert/Alert'
-import Prompt from '../../recycle/Prompt/Prompt'
+import React, { Suspense, useEffect } from "react"
+import Layout from "../Layout/Layout"
+import BurgerBuilder from "../BurgerBuilder/BurgerBuilder"
+import { Route, Switch, Redirect } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import * as actions from "../../store/actions/exports"
+import Spinner from "../../recycle/Spinner/Spinner"
+import useAlert from "../../hooks/useAlert"
+import Alert from "../../recycle/Alert/Alert"
+import Prompt from "../../recycle/Prompt/Prompt"
 
-const Checkout = React.lazy(() => import ('../Checkout/Checkout'))
-const Orders = React.lazy(() => import ('../Orders/Orders'))
-const Login = React.lazy(() => import ('../Login/Login'))
-const Signup = React.lazy(() => import ('../Signup/Signup'))
-const EditProfile = React.lazy(() => import('../EditProfile/EditProfile'))
-const StaticProfile = React.lazy(() => import('../StaticProfile/StaticProfile'))
-
+const Checkout = React.lazy(() => import("../Checkout/Checkout"))
+const Orders = React.lazy(() => import("../Orders/Orders"))
+const Login = React.lazy(() => import("../Login/Login"))
+const Signup = React.lazy(() => import("../Signup/Signup"))
+const EditProfile = React.lazy(() => import("../EditProfile/EditProfile"))
+const StaticProfile = React.lazy(() => import("../StaticProfile/StaticProfile"))
 
 const App = props => {
+	const dispatch = useDispatch()
+	const reduxToken = useSelector(state => state.login.idToken)
+	const timerExpired = useSelector(state => state.login.timerExpired)
 
-  const dispatch = useDispatch();
-  const reduxToken = useSelector(state => state.login.idToken);
-  const timerExpired = useSelector(state => state.login.timerExpired);
+	const [alertShown, showAlert] = useAlert(false)
 
-  const [alertShown, showAlert] = useAlert(false);
+	const localToken = localStorage.getItem("token")
 
-  const localToken = localStorage.getItem('token');  
+	useEffect(() => {
+		dispatch(actions.refreshAuth(localToken))
+		// eslint-disable-next-line
+	}, [])
 
-  useEffect(() => {
-      dispatch(actions.refreshAuth(localToken))
-  // eslint-disable-next-line 
-  },[])
+	const acknowledgeAlert = () => {
+		showAlert(false)
+		dispatch(actions.acknowledgeTimerExpiration())
+	}
 
+	useEffect(() => {
+		if (timerExpired) {
+			showAlert(true)
+		}
+		// eslint-disable-next-line
+	}, [timerExpired])
 
-  const acknowledgeAlert = () => {
-    showAlert(false)
-    dispatch(actions.acknowledgeTimerExpiration())
-  }
+	const isAuthenticated = reduxToken !== null
 
-  useEffect(() => {
-    if(timerExpired){
-      showAlert(true)
-    }
-  // eslint-disable-next-line
-  },[timerExpired])
+	const routes = function (isAuthenticated) {
+		if (isAuthenticated) {
+			return (
+				<>
+					<Suspense fallback={<Spinner />}>
+						<Route path="/checkout" component={Checkout} />
+					</Suspense>
+					<Suspense fallback={<Spinner />}>
+						<Route path="/orders" component={Orders} />
+					</Suspense>
+					<Suspense fallback={<Spinner />}>
+						<Route path="/profile-edit" component={EditProfile} />
+					</Suspense>
+					<Suspense fallback={<Spinner />}>
+						<Route path="/profile" component={StaticProfile} />
+					</Suspense>
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</>
+			)
+		} else {
+			return (
+				<>
+					<Suspense fallback={<Spinner />}>
+						<Route path="/login" component={Login} />
+					</Suspense>
+					<Suspense fallback={<Spinner />}>
+						<Route path="/signup" component={Signup} />
+					</Suspense>
+					<Route path="/" exact component={BurgerBuilder} />
+					<Redirect to="/" />
+				</>
+			)
+		}
+	}
 
- 
-  const isAuthenticated = reduxToken !== null;
+	return (
+		<>
+			{alertShown ? (
+				<Alert closeAlert={acknowledgeAlert}>
+					<Prompt acknowledgeAlert={acknowledgeAlert}>
+						You are automatically logged out :( Please login again
+					</Prompt>
+				</Alert>
+			) : null}
 
-
-  const routes = function (isAuthenticated){
-    if(isAuthenticated){
-      return (
-          <>
-            <Suspense fallback={<Spinner/>}><Route path="/checkout" component={Checkout}/></Suspense>
-            <Suspense fallback={<Spinner/>}><Route path="/orders" component={Orders}/></Suspense>
-            <Suspense fallback={<Spinner/>}><Route path="/profile-edit" component={EditProfile}/></Suspense>
-            <Suspense fallback={<Spinner/>}><Route path="/profile" component={StaticProfile}/></Suspense>
-            <Route path="/" exact component={BurgerBuilder}/>
-            
-          </>
-      )
-    } else {
-      return (
-        <>
-          <Suspense fallback={<Spinner/>}><Route path="/login" component={Login} /></Suspense>
-          <Suspense fallback={<Spinner/>}><Route path="/signup" component={Signup} /></Suspense>
-          <Route path="/" exact component={BurgerBuilder}/>
-          <Redirect to = "/"/>
-        </>
-      )
-    }
-  }
-
-  return(
-    <>
-      {alertShown? 
-        <Alert closeAlert={acknowledgeAlert}>
-          <Prompt acknowledgeAlert={acknowledgeAlert}>
-              You are automatically logged out :( Please login again
-          </Prompt>
-        </Alert>
-      : null}
-
-
-      <Layout>
-        <Switch>
-            {routes(isAuthenticated)}
-        </Switch>
-      </Layout>
-    </>
-  )
+			<Layout>
+				<Switch>{routes(isAuthenticated)}</Switch>
+			</Layout>
+		</>
+	)
 }
 
-export default App;
+export default App
