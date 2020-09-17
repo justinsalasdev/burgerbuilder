f0af0893c94c89e3e0162e284c3c25905dbd2fd3
l0ckstreet <rsalas7793@gmail.com>
2020-09-17 21:59:05 +0800

    edited url

--- src/index.js
+++ src/index.js
@@ -1,52 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/index.scss';
-import App from './containers/App/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import buildBurger from './store/reducers/buildBurger'
-import placeOrder from './store/reducers/placeOrder'
-import fetchOrders from './store/reducers/fetchOrders'
-import login from './store/reducers/login'
-import signup from './store/reducers/signup'
-import updateProfile from './store/reducers/updateProfile'
-import thunk from 'redux-thunk';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./assets/styles/index.scss"
+import App from "./containers/App/App"
+import * as serviceWorker from "./serviceWorker"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, combineReducers, compose } from "redux"
+import buildBurger from "./store/reducers/buildBurger"
+import placeOrder from "./store/reducers/placeOrder"
+import fetchOrders from "./store/reducers/fetchOrders"
+import login from "./store/reducers/login"
+import signup from "./store/reducers/signup"
+import updateProfile from "./store/reducers/updateProfile"
+import thunk from "redux-thunk"
 
-
-const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
+const composeEnhancers =
+	(process.env.NODE_ENV === "development"
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null) || compose
 
 const rootReducer = combineReducers({
-  buildBurger,
-  placeOrder,
-  fetchOrders,
-  login,
-  signup,
-  updateProfile
+	buildBurger,
+	placeOrder,
+	fetchOrders,
+	login,
+	signup,
+	updateProfile
 })
 
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-  applyMiddleware(thunk)
-));
-
+const store = createStore(
+	rootReducer,
+	/* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+)
 
 const app = (
-  <Provider store={store}>
-    <BrowserRouter basename="/burgerbuilder">
-      <App />
-    </BrowserRouter>
-  </Provider>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 )
 
 ReactDOM.render(
-  <React.StrictMode>
-      {app}
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+	<React.StrictMode>{app}</React.StrictMode>,
+	document.getElementById("root")
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
